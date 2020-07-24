1237b39c1c297639b0140e902a9f776530d82cc9
suyash2411 <jadhavsuyu@gmail.com>
2020-07-24 13:52:21 +0530

    Intership Task

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suyash\onedrive\desktop\project-for-dezzex.tech-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
